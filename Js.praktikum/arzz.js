678bb2eb853e7551d959bce988b708fb5f97da39
Arzzzzak <168519532+Arzzzzak@users.noreply.github.com>
2024-05-15 12:20:13 +0700

    Add files via upload

---
+++ Js.praktikum/arzz.js
@@ -0,0 +1,38 @@
+//let//
+
+let t= 81 + 24
+ t= 10 + 11
+console.log(t) 
+
+let p= 74 + 36
+ p-= 20 + 15
+console.log(p)
+
+let q= 25 - 23
+ q*= 12 + 27
+console.log(q)
+
+let w= 2 + 11
+ w= 2 + 13
+console.log(w)
+
+let d= 3 - 10
+ d= 5 + 20
+console.log(d)
+
+//var//
+
+var y= 6 + 27
+console.log(y)
+var y= 5 + 13
+console.log(y)
+var y= 10 + 32
+console.log(y)
+var y= 5 + 15
+console.log(y)
+var y= 9 + 28
+console.log(y)
+
+  
+ 
+
